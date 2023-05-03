970ef85614b380b93a9c753594ef415e8417eaa1
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-05-03 16:45:01 +0300

    Remove old construction with Response

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,4 +1,4 @@
-from requests import Response
+#from requests import Response
 from utils.api import GoogleMapsApi
 
 
@@ -7,12 +7,13 @@ class TestCreatePlace:
     def test_create_new_place(self):
 
         print("Method POST")
-        result_post: Response = GoogleMapsApi.create_new_place()
+        # result_post: Response = GoogleMapsApi.create_new_place()
+        result_post = GoogleMapsApi.create_new_place()
         check_post = result_post.json()
         place_id = check_post.get("place_id")
 
         print("Method GET")
-        result_get: Response = GoogleMapsApi.get_new_place(place_id)
+        result_get = GoogleMapsApi.get_new_place(place_id)
 
 
 
