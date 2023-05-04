a99ac5115f0f917f8c5328a0863ea68d8b981b67
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-05-04 21:39:04 +0300

    final version status code check

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,4 +1,5 @@
-#from requests import Response
+from requests import Response
+from utils.cheking import Checking
 from utils.api import GoogleMapsApi
 
 
@@ -7,22 +8,28 @@ class TestCreatePlace:
     def test_create_new_place(self):
 
         print("Method POST")
-        # result_post: Response = GoogleMapsApi.create_new_place()
+        #result_post: Response = GoogleMapsApi.create_new_place()
         result_post = GoogleMapsApi.create_new_place()
         check_post = result_post.json()
         place_id = check_post.get("place_id")
+        Checking.check_status_code(result_post, 200)
 
         print("Method GET POST")
         result_get = GoogleMapsApi.get_new_place(place_id)
+        Checking.check_status_code(result_get, 200)
 
         print("Method PUT")
         result_put = GoogleMapsApi.put_new_place(place_id)
+        Checking.check_status_code(result_put, 200)
 
         print("Method GET PUT")
         result_get = GoogleMapsApi.get_new_place(place_id)
+        Checking.check_status_code(result_get, 200)
 
         print("Method DELETE")
         result_delete = GoogleMapsApi.delete_new_place(place_id)
+        Checking.check_status_code(result_delete, 200)
 
         print("Method GET DELETE")
         result_get = GoogleMapsApi.get_new_place(place_id)
+        Checking.check_status_code(result_get, 404)
--- utils/cheking.py
+++ utils/cheking.py
@@ -1,15 +1,16 @@
 """Methods for checking the responses of our requests"""
-from requests import Response
+#from requests import Response
 
 
 class Checking:
 
     """Methods for checking the status code"""
     @staticmethod
-    def check_status_code(response: Response, status_code):
-        assert status_code == response.status_code
-        if response.status_code == status_code:
-            print("Successfully!!! Statud code:", response.status_code)
-        else:
-            print("Failure!!! Statud code:", response.status_code)
+    def check_status_code(result, status_code):
+        assert status_code == result.status_code
+        print("Successfully!!! Status code:", result.status_code)
+        # if response.status_code == status_code:
+
+        # else:
+        #     print("Failure!!! Statud code:", response.status_code)
 
