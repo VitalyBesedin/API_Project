6486143dcdf3d38eb57b948932c4599ee161f323
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-05-05 15:58:32 +0300

    getting checking fields

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,4 +1,4 @@
-from requests import Response
+import json
 from utils.cheking import Checking
 from utils.api import GoogleMapsApi
 
@@ -8,28 +8,38 @@ class TestCreatePlace:
     def test_create_new_place(self):
 
         print("Method POST")
-        #result_post: Response = GoogleMapsApi.create_new_place()
         result_post = GoogleMapsApi.create_new_place()
         check_post = result_post.json()
         place_id = check_post.get("place_id")
         Checking.check_status_code(result_post, 200)
+        Checking.check_json_token(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
 
         print("Method GET POST")
         result_get = GoogleMapsApi.get_new_place(place_id)
         Checking.check_status_code(result_get, 200)
+        token = json.loads(result_get.text)
+        print(list(token))
+
 
         print("Method PUT")
         result_put = GoogleMapsApi.put_new_place(place_id)
         Checking.check_status_code(result_put, 200)
+        Checking.check_json_token(result_put, ['msg'])
 
         print("Method GET PUT")
         result_get = GoogleMapsApi.get_new_place(place_id)
         Checking.check_status_code(result_get, 200)
+        token = json.loads(result_get.text)
+        print(list(token))
 
         print("Method DELETE")
         result_delete = GoogleMapsApi.delete_new_place(place_id)
         Checking.check_status_code(result_delete, 200)
+        token = json.loads(result_delete.text)
+        print(list(token))
 
         print("Method GET DELETE")
         result_get = GoogleMapsApi.get_new_place(place_id)
         Checking.check_status_code(result_get, 404)
+        token = json.loads(result_get.text)
+        print(list(token))
