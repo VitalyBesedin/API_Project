fbedd052cdebca880e72a9f41bf36e1cdb7cff29
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-05-07 22:52:47 +0300

    next logger's version

--- utils/logger.py
+++ utils/logger.py
@@ -3,8 +3,7 @@ import os
 
 
 class Logger:
-    file_name = f"C:\\Users\\Acer\\PycharmProjects\\main_project\\logs\\log_" + \
-                str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")) + ".log"
+    file_name = f"\logs\log_" + str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")) + ".log"
 
     @classmethod
     def write_log_to_file(cls, data: str):
@@ -12,23 +11,38 @@ class Logger:
             logger_file.write(data)
 
     @classmethod
-    def add_start_step(cls, method: str):
+    def add_start_step(cls, url: str, method: str):
         test_name = os.environ.get('PYTEST_CURRENT_TEST')
 
         data_to_add = f"\n-----\n"
         data_to_add += f"Test: {test_name}\n"
-        data_to_add += f"Start time: {str(datetime.datetime.now())}\n"
-        data_to_add += f"Start name method: {method}\n"
+        data_to_add += f"Time: {str(datetime.datetime.now())}\n"
+        data_to_add += f"Request method: {method}\n"
+        data_to_add += f"Request URL: {url}\n"
         data_to_add += "\n"
 
         cls.write_log_to_file(data_to_add)
 
     @classmethod
-    def add_end_step(cls, url: str, method: str):
+    def add_response(cls, result):
+        cookies_as_dict = dict(result.cookies)
+        headers_as_dict = dict(result.headers)
 
-        data_to_add = f"End time: {str(datetime.datetime.now())}\n"
-        data_to_add += f"End name method: {method}\n"
-        data_to_add += f"URL: {url}\n"
+        data_to_add = f"Response code: {result.statuse_code}\n"
+        data_to_add += f"Response text: {result.text}\n"
+        data_to_add += f"Response headers: {headers_as_dict}\n"
+        data_to_add += f"Response cookies: {cookies_as_dict}\n"
         data_to_add += f"\n-----\n"
 
         cls.write_log_to_file(data_to_add)
+
+
+    # @classmethod
+    # def add_end_step(cls, url: str, method: str):
+    #
+    #     data_to_add = f"End time: {str(datetime.datetime.now())}\n"
+    #     data_to_add += f"End name method: {method}\n"
+    #     data_to_add += f"URL: {url}\n"
+    #     data_to_add += f"\n-----\n"
+    #
+    #     cls.write_log_to_file(data_to_add)
